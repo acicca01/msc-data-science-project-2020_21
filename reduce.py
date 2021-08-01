7667493f997d4aed7d8b944818a786642676ede8
Antonello Ciccarone <acicca01@dcs.bbk.ac.uk>
2021-08-01 11:02:22 +0100

    Apply pre-processing prior to PCA

--- reduce.py
+++ reduce.py
@@ -1,21 +1,24 @@
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+import os
+
+scaler = StandardScaler()
+pca = PCA(n_components=8)
 def reduce(source="/Users/pantera/melon/arena_mel"):
-    i = 0
     for root, dirs, files in os.walk(source, topdown=False):
         for name in files:
-            i +=1
-            if i <10:
-                source = os.path.join(root, name)
-                destination = source.replace("arena_mel","arena_mel_compress")
-                tmp = np.load(source)
-                pca = PCA(n_components=10)
-                pca.fit(tmp)
-                tmp2 = pca.transform(tmp)
-                try:
-                    np.save(destination, tmp2, allow_pickle=True, fix_imports=True)
-                except: 
-                    ddir = destination[:(len(destination)-len(name)-1)]
-                    os.mkdir(ddir)
-                    np.save(destination, tmp2, allow_pickle=True, fix_imports=True)
-            else:
-                break
+            source = os.path.join(root, name)
+            destination = source.replace("arena_mel","arena_mel_compress")
+            tmp = np.load(source)
+            tmp = tmp.astype(np.double)
+            tmp_scaled = scaler.fit_transform(tmp)
+            tmp2 = pca.fit_transform(tmp_scaled)
+            try:
+                np.save(destination, tmp2, allow_pickle=True, fix_imports=True)
+            except: 
+                ddir = destination[:(len(destination)-len(name)-1)]
+                os.mkdir(ddir)
+                np.save(destination, tmp2, allow_pickle=True, fix_imports=True)
+
         
