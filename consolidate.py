93e7088aed3c66192d661d6dbca08a5a1f12be6a
Antonello Ciccarone <acicca01@dcs.bbk.ac.uk>
2021-08-09 21:04:08 +0100

    Add try-except block for failed songs

--- consolidate.py
+++ consolidate.py
@@ -1,8 +1,11 @@
-def consolidate(songs: list,allin: str,root= "/Users/pantera/melon/arena_mel_compress/"):
+def consolidate(songs: list,allin: str,root= "/Users/pantera/melon/arena_mel/"):
     """Recursively look for songs data in root and consolidate them into a single file allin. The consolidated file is saved in root"""
     bigone = []
     for song in songs:
         loadthis = os.path.join(root,str(math.floor(song/1000)),str(song)+".npy")
         bigone.append(np.load(loadthis))
-    bigonenp = np.array(bigone)
-    np.save(root+allin,bigonenp)    
+    try:    
+        bigonenp = np.asarray(bigone)
+        np.save(root+allin,bigonenp)
+    except:
+        print(song , "Failed")
