b28a91d8fd6ddd9a577a1171b33f15d361c25a43
Antonello Ciccarone <acicca01@dcs.bbk.ac.uk>
2021-08-09 21:07:28 +0100

    Add try-except block for failed songs

--- reduce.py
+++ reduce.py
@@ -8,10 +8,13 @@ def reduce(components:int,source="/Users/pantera/melon/arena_mel"):
         for name in files:
             source = os.path.join(root, name)
             destination = source.replace("arena_mel","arena_mel_compress")
-            tmp = np.load(source)
-            tmp = tmp.astype(np.double)
-            tmp_scaled = scaler.fit_transform(tmp)
-            tmp2 = pca.fit_transform(tmp_scaled)
+            try:
+                tmp = np.load(source)
+                tmp = tmp.astype(np.double)
+                tmp_scaled = scaler.fit_transform(tmp)
+                tmp2 = pca.fit_transform(tmp_scaled)
+            except:
+                print(name, "failed")
             try:
                 np.save(destination, tmp2, allow_pickle=True, fix_imports=True)
             except: 
