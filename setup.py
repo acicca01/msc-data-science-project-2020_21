da949bf104203b2853d905e7bfae17ec75bc6167
Antonello Ciccarone <acicca01@dcs.bbk.ac.uk>
2021-08-23 07:36:03 +0100

    Updated package version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="MSc",
-    version="0.0.1",
+    version="0.0.2",
     author="Antonello Ciccarone",
     description="MSc Project code",
     long_description=long_description,
