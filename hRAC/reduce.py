df6b313bf906ddb02a94b9fcfa07d541f3b708ba
Antonello Ciccarone <acicca01@dcs.bbk.ac.uk>
2021-08-23 07:32:23 +0100

    Refactoring

--- hRAC/reduce.py
+++ hRAC/reduce.py
@@ -1,4 +1,4 @@
-def reduce(components:int,source="/Users/pantera/melon/arena_mel"):
+def reduce(components:int,target: str,source="/Users/pantera/melon/arena_mel"):
     """Dimensionality reduction with PCA, from 1876 down to components"""
     from sklearn.decomposition import PCA
     from sklearn.preprocessing import StandardScaler
@@ -6,8 +6,12 @@ def reduce(components:int,source="/Users/pantera/melon/arena_mel"):
     pca = PCA(n_components=components)
     for root, dirs, files in os.walk(source, topdown=False):
         for name in files:
+            #source file to compress
             source = os.path.join(root, name)
-            destination = source.replace("arena_mel","arena_mel_compress")
+            #destination directory 
+            destdir =os.path.join(target,source.split('/')[-2])
+            #full path to target file
+            target_file =  os.path.join(destdir,name)
             try:
                 tmp = np.load(source)
                 tmp = tmp.astype(np.double)
@@ -16,9 +20,8 @@ def reduce(components:int,source="/Users/pantera/melon/arena_mel"):
             except:
                 print(name, "failed")
             try:
-                np.save(destination, tmp2, allow_pickle=True, fix_imports=True)
+                np.save(target_file, tmp2, allow_pickle=True, fix_imports=True)
             except: 
-                ddir = destination[:(len(destination)-len(name)-1)]
-                os.mkdir(ddir)
-                np.save(destination, tmp2, allow_pickle=True, fix_imports=True)
+                os.mkdir(destdir)
+                np.save(target_file, tmp2,allow_pickle=True, fix_imports=True)
 
