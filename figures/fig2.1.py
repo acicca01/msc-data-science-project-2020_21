c4867e58523de1fb7811ae7250e053f7dadb80e0
Antonello Ciccarone <acicca01@dcs.bbk.ac.uk>
2021-08-29 18:52:20 +0100

    Display tracks popularity distribution

---
+++ figures/fig2.1.py
@@ -0,0 +1,31 @@
+#import libraries
+import pandas as pd
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+from math import log
+
+#open dictionary with track-playlist interaction and define popularity as number of playlists listening to track 
+with open('/Users/pantera/melon/tracks_dict.pickle', 'rb') as handle:
+    tracks_dict = pickle.load(handle)
+popularity = {x : len(v) for x,v in tracks_dict.items() }
+popdata = np.zeros(len(popularity))
+i = 0
+for k in popularity.keys():
+    popdata[i]=popularity[k]
+    i+=1
+
+#function to log-equally space consecutive bin edges 
+def logbins(data,n):
+    return np.logspace(np.log10(min(data)),np.log10(max(data)),n)
+
+#display power law in dataset
+fig, ax = plt.subplots(figsize = (7,5))
+ax.set_ylabel("Track Popularity")
+ax.set_xlabel("Number of tracks")
+plt.xscale('log')
+plt.yscale('log')
+ax.tick_params(left = False, bottom = False)
+ 
+hist, bins, _  = plt.hist(popdata,bins=logbins(popdata,10),facecolor='g', alpha=0.55 )
+
