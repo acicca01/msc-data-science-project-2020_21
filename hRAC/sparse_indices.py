9316aa5d4c7d1cd261574b30785428a41ba2ca24
Antonello Ciccarone <acicca01@dcs.bbk.ac.uk>
2021-09-05 13:50:27 +0100

    Adapted sparse_indices function for List data type

--- hRAC/sparse_indices.py
+++ hRAC/sparse_indices.py
@@ -1,12 +1,12 @@
-def sparse_indices(interactions: dict):
+def sparse_indices(interactions: list):
     """ Returns a triplet of parameters that can be used to fill in a sparse matrix (Playlist-Track interactions  utility matrix)"""
 
     data = []
     row_ind = []
     col_ind = []
-    for k,v in interactions.items():
-        for playlist in v:
+    for i in range(len(interactions)):
+        for playlist in interactions[i]:
             data.append(1)
             row_ind.append(playlist)
-            col_ind.append(k)
+            col_ind.append(i)
     return data, row_ind, col_ind
