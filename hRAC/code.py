6a5b84e39abc60bf3a675a9ba7f3da9684ce6351
Antonello Ciccarone <acicca01@dcs.bbk.ac.uk>
2021-09-13 18:00:32 +0100

    Enabled default dump to pickle

--- hRAC/code.py
+++ hRAC/code.py
@@ -1,8 +1,12 @@
-def code(idlist , mode = "EN"):
-    """ Encode or Decode Playlists/Track ids to a contiguous sequence """ 
-    if mode == 'EN':
-        #idlist_to_sequence 
-        return {y :x  for (x,y) in enumerate(idlist)}  
-    if mode == "DE":    
-        #sequence_to_idlist 
-        return {x :y  for (x,y) in enumerate(idlist)}
+def code(idlist , files= "/Users/pantera/melon/files/"):
+    """ Encode or Decode Playlists/Track ids to a contiguous sequence
+        It saves the encodemap and decodemap in files                """
+    #idlist_to_sequence
+    encodemap = {y :x  for (x,y) in enumerate(idlist)}
+    with open(os.path.join(files,"encodemap"), 'wb') as handle:
+        pickle.dump(encodemap, handle)
+    #sequence_to_idlist
+    decodemap = {x :y  for (x,y) in enumerate(idlist)}
+    with open(os.path.join(files,"decodemap"), 'wb') as handle:
+        pickle.dump(decodemap, handle)
+    return (encodemap,decodemap)
