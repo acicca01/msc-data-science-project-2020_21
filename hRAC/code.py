c66f18a11e606083e5d32e48f1ec6604ec3dabec
Antonello Ciccarone <acicca01@dcs.bbk.ac.uk>
2021-09-15 09:25:30 +0100

    Added parameter for filename

--- hRAC/code.py
+++ hRAC/code.py
@@ -1,12 +1,14 @@
-def code(idlist , files= "/Users/pantera/melon/files/"):
+def code(idlist ,filename ,files= "/Users/pantera/melon/files/"):
+    import os
+    import pickle 
     """ Encode or Decode Playlists/Track ids to a contiguous sequence
         It saves the encodemap and decodemap in files                """
     #idlist_to_sequence
     encodemap = {y :x  for (x,y) in enumerate(idlist)}
-    with open(os.path.join(files,"encodemap"), 'wb') as handle:
+    with open(os.path.join(files,filename+".pickle"), 'wb') as handle:
         pickle.dump(encodemap, handle)
     #sequence_to_idlist
     decodemap = {x :y  for (x,y) in enumerate(idlist)}
-    with open(os.path.join(files,"decodemap"), 'wb') as handle:
+    with open(os.path.join(files,filename+".pickle"), 'wb') as handle:
         pickle.dump(decodemap, handle)
     return (encodemap,decodemap)
