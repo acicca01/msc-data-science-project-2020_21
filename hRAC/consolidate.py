598f037816751f760c97a5ce50ca5e1b506e7c2d
Antonello Ciccarone <acicca01@dcs.bbk.ac.uk>
2021-09-15 08:47:07 +0100

    Code Refactoring

--- hRAC/consolidate.py
+++ hRAC/consolidate.py
@@ -1,5 +1,8 @@
 def consolidate(songs: list,allin: str,root= "/Users/pantera/melon/arena_mel/",files = "/Users/pantera/melon/files/" ):
     """Recursively look for songs data in root and consolidate them into a single file allin. The consolidated file is saved in root"""
+    import os
+    import math
+    import numpy as np
     bigone = []
     for song in songs:
         loadthis = os.path.join(root,str(math.floor(song/1000)),str(song)+".npy")
