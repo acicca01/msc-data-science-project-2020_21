dff573fa0097cae420c5109b23d096f6af8e473b
Antonello Ciccarone <acicca01@dcs.bbk.ac.uk>
2021-09-16 20:44:40 +0100

    Making code leaner

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 from hRAC.code import code
 from hRAC.sparse_indices import sparse_indices
-from hRAC.takedown import takedown
+from hRAC.eval import eval
+from hRAC.sparsify import sparsify
 import random
 import math
 import pandas as pd
@@ -9,8 +10,11 @@ import numpy as np
 import pickle
 from implicit.lmf import LogisticMatrixFactorization
 from scipy.sparse import csr_matrix
-from sklearn import metrics
 import time
+
+#location for intermediate files drop. Useful when switching environment for CNN training
+files = "/Users/pantera/melon/files"
+
 #read in the data and discover playlists-track interaction
 train = pd.read_json('/Users/pantera/melon/train.json')
 train2 = train[['id','songs']]
@@ -35,103 +39,61 @@ with open('/Users/pantera/melon/tracks_dict.pickle', 'rb') as handle:
 alltracks = [x for x in tracks_dict.keys()]
 allplaylists = [tracks for playlist in tracks_dict.values() for tracks in playlist]
 allplaylists=list(set(allplaylists))
-trackmap = code(alltracks)[0]
-playlistmap = code(allplaylists)[0]
+trackmap = code(alltracks,"tracks")[0]
+playlistmap = code(allplaylists,"playlists")[0]
 tracks_dict_map={trackmap[k]: [playlistmap[pl_id] for pl_id in v] for k ,v in tracks_dict.items()}
 
 
-#now we can deal with arrays
-traindata = np.asarray([ v for _,v in tracks_dict_map.items() ],dtype=object)
 #Encoding original "train" dataframe for reference
 train_coded = train3.assign(id_encoded = train3["id"].map(playlistmap))
 train_coded = train_coded.assign(tracks_encoded = train_coded["tracks"].map(lambda v : [trackmap[x] for x in v]))
 plays_dict_map = {train_coded['id_encoded'][i] : train_coded['tracks_encoded'][i]  for i in range(train_coded.shape[0]) }
-#get popularity
-popularity = [len(x) for x in traindata]
 
-#masking cold start tracks (i.e. less than 30 interactions) 
-#for those tracks drop 50% of the interactions at random 
-#!! cold start tracks with only 1 interaction will be spared from dropping
+#now we can deal with arrays
+testdata = list(range(len(alltracks)))
 
-#testdata = traindata.copy()
-mask = [0 < interactions < 31 for interactions in popularity]
-wipepop = [31<interactions for interactions in popularity]
-#save results for later loading into audio module
-with open('/Users/pantera/melon/files/coldtracks.pickle', 'wb') as handle:
-    pickle.dump(mask, handle)
-with open('/Users/pantera/melon/files/poptracks.pickle', 'wb') as handle:
-    pickle.dump(wipepop handle)
+#testdata will contain all original interactions
+for k,v in tracks_dict_map.items():
+    testdata[k] = v
 
-traindata = np.asarray([ v for _,v in tracks_dict_map.items() ],dtype=object)
-for i in range(len(traindata)):
-    if wipepop[i] == True:
-        traindata[i] = []
-testdata = traindata.copy()
+#userdata to check user activity. Less active users will be dropped
+userdata = list(range(len(allplaylists)))
+for k,v in plays_dict_map.items():
+    userdata[k] = v
 
+#get user activity
+activity = [len(x) for x in userdata]
 
-for i in range(len(traindata)):
-    if mask[i] == True:
-        traindata[i] = random.sample(traindata[i],int(len(traindata[i])*0.5))
+config0 = [2,10,30]
+config1 = [10,30,50,100]
 
-#check percentages of interactions masked
-#interaction of j-th column i-th row . Column = tracks , row = playlist
-#interactionstrain = ( (j,i) for j in range(len(traindata)) for i in traindata[j])
-#interactionstest = ( (j,i) for j in range(len(testdata)) for i in testdata[j])
-#(1 - (len([x for x in interactionstrain])/len([x for x in interactionstest])))*100
+results = []
+for cfg0 in config0:
+    np.random.seed(0)
+    #mask interactions
+    sparsify_out = sparsify(cfg0,testdata)
+    print("{}% interactions dropped from test".format(sparsify_out['drop_pct']) )
+    #get parameters to build hot and cold sparse matrices of interactions
+    train_idx = sparse_indices(sparsify_out['traindata'])
 
-#build a list so that masked_playlists[i] gives the tracks that should be recommended to playlist i
-masked_playlists = [ [] for i in range(len(allplaylists))]
-for i in range(len(traindata)):
-    if mask[i] == True:
-        for j in range(len(testdata[i])):
-            # testdata[i][j] gives me playlist id being masked
-            if testdata[i][j] not in traindata[i]:
-                masked_playlists[testdata[i][j]].append(i)
+    test_idx = sparse_indices(testdata)
+    #build interaction matrixes
+    M = len(allplaylists) 
+    N = len(alltracks)
 
-#for each playlist give me tracks exposed to training data
-exposed_playlists = [ [] for i in range(len(allplaylists))]                
-for i in range(len(traindata)):
-    for j in range(len(traindata[i])):
-        exposed_playlists[traindata[i][j]].append(i)
+    train_sparse  = csr_matrix((train_idx[0], (train_idx[1], train_idx[2])),shape=(M,N))
+    test_sparse  = csr_matrix((test_idx[0], (test_idx[1], test_idx[2])),shape=(M,N))
 
-#get parameters to build hot and cold sparse matrices of interactions
-train_idx = sparse_indices(traindata)
-test_idx = sparse_indices(testdata)
-#build interaction matrixes
-M = len(allplaylists) 
-N = len(alltracks)
-
-#for model fitting I need to pass the transposed of user-item activity matrix
-train_sparse  = csr_matrix((train_idx[0], (train_idx[1], train_idx[2])),shape=(M,N))
-#leaving test data as it is (user-items)
-test_sparse  = csr_matrix((test_idx[0], (test_idx[1], test_idx[2])),shape=(M,N))
-#implicit 
-logmodel = LogisticMatrixFactorization(factors=30, iterations=40, regularization=1.5)
-logmodel.fit(train_sparse.T)
-
-#evaluation
-#filter out users who did not have any item masked
-modplaylist = [i for i in range(len(masked_playlists)) if len(masked_playlists[i]) != 0]
-start = time.time()
-auc_scores = []
-for i in modplaylist:
-    actual = test_sparse[i,:].toarray().reshape(-1)
-    actual_less = np.delete(actual,exposed_playlists[i])
-    scores = logmodel.user_factors[i,:].dot(logmodel.item_factors.T)
-    scores_less = np.delete(scores,exposed_playlists[i])
-    fpr, tpr, thresholds = metrics.roc_curve(actual_less, scores_less)
-    auc_scores.append(metrics.auc(fpr, tpr))
-print("generated scores in {} minutes ".format(int(time.time() -start)/60))
+    #for model fitting I need to pass the transposed of user-item activity matrix
+    logmodel = LogisticMatrixFactorization(factors=32, iterations=40, regularization=1.5)
+    logmodel.fit(train_sparse.T)
+    for cfg1 in config1:
+        #saving track factors . These will be used as target variable for training the CNN 
+        #np.save(os.path.join(files,"track_factors{}_{}.npy".format(cfg[0],cfg[1])),logmodel.item_factors)
+        evallist = list(set([ele[0] for ele in sparsify_out['masked'] if activity[ele[0]] > cfg1 ]))
+        checklist = np.random.choice(evallist,100,replace = False)
+        x = eval(train_sparse,test_sparse,logmodel.user_factors,logmodel.item_factors,checklist)
+        results.append(((cfg0,cfg1),sparsify_out['drop_pct'],sparsify_out['sparsity_pct'],x)) 
+print(results)
 
 
-
-
-
-#compare lists
-def cl(base,compare):
-    match = 0
-    for ele in compare:
-        for ele2 in base:
-              if ele == ele2:
-                  match+=1
-    return match
