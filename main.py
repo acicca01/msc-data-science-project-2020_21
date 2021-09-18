da5e96208f74538ec3cf8d57679e19b954b65dfe
Antonello Ciccarone <acicca01@dcs.bbk.ac.uk>
2021-09-18 21:29:19 +0100

    Added topN popular tracks block for CNN training

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ import numpy as np
 import pickle
 from implicit.lmf import LogisticMatrixFactorization
 from scipy.sparse import csr_matrix
+from numpy.random import default_rng
 import time
+
 
 #location for intermediate files drop. Useful when switching environment for CNN training
 files = "/Users/pantera/melon/files"
@@ -39,7 +41,7 @@ with open('/Users/pantera/melon/tracks_dict.pickle', 'rb') as handle:
 alltracks = [x for x in tracks_dict.keys()]
 allplaylists = [tracks for playlist in tracks_dict.values() for tracks in playlist]
 allplaylists=list(set(allplaylists))
-trackmap = code(alltracks,"tracks")[0]
+trackmap,decode = code(alltracks,"tracks")
 playlistmap = code(allplaylists,"playlists")[0]
 tracks_dict_map={trackmap[k]: [playlistmap[pl_id] for pl_id in v] for k ,v in tracks_dict.items()}
 
@@ -49,7 +51,9 @@ train_coded = train3.assign(id_encoded = train3["id"].map(playlistmap))
 train_coded = train_coded.assign(tracks_encoded = train_coded["tracks"].map(lambda v : [trackmap[x] for x in v]))
 plays_dict_map = {train_coded['id_encoded'][i] : train_coded['tracks_encoded'][i]  for i in range(train_coded.shape[0]) }
 
-#now we can deal with arrays
+#now that we have sequential IDs for playlists and tracks we can store interactions as arrays
+
+#index i represent a trackID, testdata[i] is a list of playlist IDs the track interacts with
 testdata = list(range(len(alltracks)))
 
 #testdata will contain all original interactions
@@ -63,37 +67,65 @@ for k,v in plays_dict_map.items():
 
 #get user activity
 activity = [len(x) for x in userdata]
+#get tracks popularity
+popularity = [len(x) for x in testdata]
 
-config0 = [2,10,30]
-config1 = [10,30,50,100]
+rng = default_rng()
+seed = rng.integers(100000000)
+sparsify(testdata,10,seed)
+#mask interactions
+sparsify_out = sparsify(testdata,10,seed)
+print("Sparsify using seed {} ".format(seed) )
+print()
 
-results = []
-for cfg0 in config0:
-    np.random.seed(0)
-    #mask interactions
-    sparsify_out = sparsify(cfg0,testdata)
-    print("{}% interactions dropped from test".format(sparsify_out['drop_pct']) )
-    #get parameters to build hot and cold sparse matrices of interactions
-    train_idx = sparse_indices(sparsify_out['traindata'])
+#popularity stats about dropped tracks 
+sampled_track = []
+for ele in sparsify_out["trackdropped"]:
+    sampled_track.append(popularity[ele])
 
-    test_idx = sparse_indices(testdata)
-    #build interaction matrixes
-    M = len(allplaylists) 
-    N = len(alltracks)
+spop = pd.Series(sampled_track)
+print(spop.describe())
 
-    train_sparse  = csr_matrix((train_idx[0], (train_idx[1], train_idx[2])),shape=(M,N))
-    test_sparse  = csr_matrix((test_idx[0], (test_idx[1], test_idx[2])),shape=(M,N))
+#activity stats about affected users
+sampled_user = []
+for ele in sparsify_out["userdropped"]:
+    sampled_user.append(activity[ele])
 
-    #for model fitting I need to pass the transposed of user-item activity matrix
-    logmodel = LogisticMatrixFactorization(factors=32, iterations=40, regularization=1.5)
-    logmodel.fit(train_sparse.T)
-    for cfg1 in config1:
-        #saving track factors . These will be used as target variable for training the CNN 
-        #np.save(os.path.join(files,"track_factors{}_{}.npy".format(cfg[0],cfg[1])),logmodel.item_factors)
-        evallist = list(set([ele[0] for ele in sparsify_out['masked'] if activity[ele[0]] > cfg1 ]))
-        checklist = np.random.choice(evallist,100,replace = False)
-        x = eval(train_sparse,test_sparse,logmodel.user_factors,logmodel.item_factors,checklist)
-        results.append(((cfg0,cfg1),sparsify_out['drop_pct'],sparsify_out['sparsity_pct'],x)) 
-print(results)
+sact = pd.Series(sampled_user)
+print(sact.describe())
+
+#get parameters to build hot and cold sparse matrices of interactions
+train_idx = sparse_indices(sparsify_out['traindata'])
+test_idx = sparse_indices(testdata)
+#build interaction matrixes
+M = len(allplaylists) 
+N = len(alltracks)
+train_sparse  = csr_matrix((train_idx[0], (train_idx[1], train_idx[2])),shape=(M,N))
+test_sparse  = csr_matrix((test_idx[0], (test_idx[1], test_idx[2])),shape=(M,N))
+
+#for model fitting I need to pass the transposed of user-item activity matrix
+logmodel = LogisticMatrixFactorization(factors=32, iterations=40, regularization=1.5)
+logmodel.fit(train_sparse.T)
+
+
+#export indexes for popular tracks 
+trackrank = [(i,k) for i,k in enumerate(popularity)]
+trackrank.sort(key=lambda x :x[1],reverse = True)
+traincnn=[]
+for ele in trackrank:
+    if ele[1] == 100:
+        break
+    else:
+        traincnn.append(ele[0])
+print("Extracted top {}  popular tracks for CNN".format(len(traincnn)))
+
+#decode trackids to allow proper retrieval of audio files
+traincnn_decoded  = [decode[x] for x in traincnn]
+with open('/Users/pantera/melon/files/traincnn_decoded.pickle', 'wb') as handle:
+    pickle.dump(traincnn_decoded, handle)
+#export factors for popular tracks to be used as class variables for CNN training
+
+np.save(os.path.join(files,"traincnn_factors.npy"),logmodel.item_factors[traincnn])
+#x = eval(train_sparse,test_sparse,logmodel.user_factors,logmodel.item_factors,sparsify_out["userdropped"])
 
 
