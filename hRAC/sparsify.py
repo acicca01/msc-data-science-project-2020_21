21b379074c7065adc3dec8583afbdb67b87b7560
Antonello Ciccarone <acicca01@dcs.bbk.ac.uk>
2021-09-20 20:07:09 +0100

    Fixed bug where retained entries were non correctly randomized

--- hRAC/sparsify.py
+++ hRAC/sparsify.py
@@ -1,40 +1,49 @@
-def sparsify(cutoff,data  ,files ="/Users/pantera/melon/files"):
-    """ Mask interactions in data accoring to popularity cutoff """
-    """ Returns traindata ,a list of masked interactions and percentage drop interactions  """
-    import pickle
-    import os
+def sparsify(testdata,pct,seed):
+    """
+    Drops interactions from a dataset.
+    
+    Parameters
+    ----------
+    testdata : data for original interactions passed as a list of lists     
+    pct : percentage of interactions to be dropped
+    seed : random seed
+
+    Returns
+    -------
+    It returns a dictionary with following key-value pairs
+
+    trainingdata : sparsified dataset
+    userdropped :  list of users affected from the dropping
+    trackdropped : list of dropped tracks 
+    
+    Notes
+    -----
+    (testdata[i] = list of playlistIDs interacting with trackID i)
+    """
+    import numpy as np
+    np.random.seed(seed)
     _return = dict()
-    testdata = data.copy()
-    traindata = list(range(len(testdata)))
-    #get track popularity
-    popularity = [len(x) for x in testdata]
-    #masking cold start tracks (i.e. <= cutoff interactions)
-    mask = [0 <= interactions <= cutoff for interactions in popularity]
-    wipepop = [cutoff<interactions for interactions in popularity]
-    #save results for later loading into audio module
-    with open(os.path.join(files,"coldtracks.pickle"), 'wb') as handle:
-        pickle.dump(mask, handle)
-    with open(os.path.join(files,'poptracks.pickle'), 'wb') as handle:
-        pickle.dump(wipepop, handle)
-    masked = []
-    #building train dataset
-    for track in range(len(testdata)):
-        if mask[track] == True:
-            #keep a record of masked interactions. This way we know which users had their tracks masked in training.
-            for playlist in testdata[track]:
-                masked.append((playlist,track))
-            traindata[track]=[]
-        else:
-            traindata[track] = testdata[track]
-    with open(os.path.join(files,'masked_interactions.pickle'), 'wb') as handle:
-        pickle.dump(masked, handle)
-    #check density
-    interactionstest = len([ (j,i) for j in range(len(testdata)) for i in range(len(testdata[j])) ])
-    # 70,785,005,082 is total number of elements in user-item activity matrix
-    sparsity = 1-len([ (j,i) for j in range(len(traindata)) for i in range(len(traindata[j])) ])/70785005082
-    drop_pct = (len(masked)/interactionstest)*100
-    _return['traindata'] = traindata
-    _return['masked'] = masked
-    _return['drop_pct'] = drop_pct
-    _return['sparsity_pct'] = sparsity*100
-    return _return
+    #random mask of interactions
+    interactionset = [ (user,item) for item in range(len(testdata)) for user in testdata[item] ]
+    randomindices = np.random.choice(len(interactionset),int(len(interactionset)*((100-pct)/100)) , replace = False)
+    interactionmask = []
+    #for i in range(len(randomindices)):
+    #    interactionmask.append(interactionset[i])
+    #trainingdata = []
+    for idx in randomindices:
+        interactionmask.append(interactionset[idx])
+    trainingdata = []    
+    for i in range(len(testdata)):
+        trainingdata.append([])
+    for ele in interactionmask:
+        trainingdata[ele[1]].append(ele[0])
+    A = set(interactionset)
+    B = set(interactionmask)
+    dropped = list(A-B)
+    userdropped = list(set([x[0] for x in dropped]))
+    trackdropped = list(set([x[1] for x in dropped]))
+    _return['traindata']=trainingdata
+    _return['userdropped']=userdropped
+    _return['trackdropped']=trackdropped
+    return _return 
+    
