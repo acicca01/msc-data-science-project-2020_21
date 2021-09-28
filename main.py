abf8d959d6a95a0987d6b31a59c5d8c5efb97725
Antonello Ciccarone <acicca01@dcs.bbk.ac.uk>
2021-09-28 09:59:50 +0100

    Improved output handling of shared resources

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
+import random
 from hRAC.code import code
 from hRAC.sparse_indices import sparse_indices
 from hRAC.eval import eval
 from hRAC.sparsify import sparsify
+from hRAC.fetch_mel import fetch_mel
 import random
 import math
 import pandas as pd
@@ -75,8 +77,10 @@ popularity = [len(x) for x in testdata]
 
 rng = default_rng()
 #seed = rng.integers(100000000)
-seed = 5132700
-sparsify(testdata,10,seed)
+seed = 98831045
+with open (os.path.join(files,"seed.pickle"),'wb') as handle:
+    pickle.dump(seed,handle)
+
 #mask interactions
 sparsify_out = sparsify(testdata,10,seed)
 print("Sparsify using seed {} ".format(seed) )
@@ -137,43 +141,73 @@ np.save(os.path.join(files,"user_factors_{}".format(seed)),logmodel.user_factors
 popularity_train = [len(x) for x in sparsify_out["traindata"]]
 trackrank = [(i,k) for i,k in enumerate(popularity_train)]
 trackrank.sort(key=lambda x :x[1],reverse = True)
-traincnn=[]
+tracks_desired = sparsify_out["trackdropped"]
+
+#building CNN training set
+cutoff = 0
 for ele in trackrank:
+    cutoff+=1
     if ele[1] == 100:
         break
-    else:
-        traincnn.append(ele[0])
-print("Extracted top {}  popular tracks for CNN".format(len(traincnn)))
+traincnn = []
+i=cutoff
+while len(traincnn) < 8000:
+    if trackrank[i][0] not in tracks_desired:
+        traincnn.append(trackrank[i][0])
+    i+=1
 
-#decode trackids to allow proper retrieval of audio files
-traincnn_decoded  = [decode[x] for x in traincnn]
-#exporting mel-spectrograms for CNN training
-audio_files = []
-for i in range(len(traincnn_decoded)):
-    audio_files.append(os.path.join(root,str(math.floor(traincnn_decoded[i]/1000)),str(traincnn_decoded[i])+".npy"))
-tmp = map(np.load,audio_files)
+
+#building CNN validation set
+valcnn = []
+i = cutoff + 8001
+while len(valcnn) < 2000:
+
+    if trackrank[i][0] not in tracks_desired:
+        valcnn.append(trackrank[i][0])
+    i+=1
+
+#saving train and validation sets files (including latent factors) to disk for CNN
+audio,trainidx = fetch_mel(traincnn)
+tmp = map(np.load,audio)
 trainset = np.stack(list(tmp))
 np.save(os.path.join(files,"trainset{}.npy".format(seed)),trainset)
+xport_factors = []
+for ele in trainidx:
+    xport_factors.append(traincnn_factors[ele])
+xport_factors_np = np.asarray(xport_factors)
+np.save(os.path.join(files,"trainset_factors{}.npy".format(seed)),xport_factors_np)
+
+
+audio,validx = fetch_mel(valcnn)
+tmp = map(np.load,audio)
+valset = np.stack(list(tmp))
+np.save(os.path.join(files,"valset{}.npy".format(seed)),valset)
+xport_factors = []
+for ele in validx:
+    xport_factors.append(traincnn_factors[ele])
+xport_factors_np = np.asarray(xport_factors)
+np.save(os.path.join(files,"valset_factors{}.npy".format(seed)),xport_factors_np)
+
 
 #exporting mel-spectrograms for CNN testing
+tracks_to_test = [x for x in tracks_desired if x not in valcnn+traincnn]
+tracks_to_test_random = random.sample(tracks_to_test,5000)
 
-tracks_to_test = [decode[x] for x in sparsify_out["trackdropped"]]
-test_files = []
-for i in range(len(tracks_to_test)):
-    test_files.append(os.path.join(root,str(math.floor(tracks_to_test[i]/1000)),str(tracks_to_test[i])+".npy"))
-with open(os.path.join(files,"test_files{}".format(seed)), 'wb') as handle:
-    pickle.dump(test_files, handle)
+audio,testidx = fetch_mel(tracks_to_test_random)
+tmp = map(np.load,audio)
+testset = np.stack(list(tmp))
+np.save(os.path.join(files,"testset{}.npy".format(seed)),testset)
 
-
-#export factors for popular tracks to be used as class variables for CNN training
+#exporting testset tracks latent factors for CNN evaluation
 xport_factors = []
-for ele in traincnn:
-    xport_factors.append(logmodel.item_factors[ele])
-xport_factors_np = np.asarray(xport_factors)   
-np.save(os.path.join(files,"traincnn_factors_{}.npy".format(seed)),xport_factors_np)
+for ele in testidx:
+    xport_factors.append(item_factors[ele])
+xport_factors_np = np.asarray(xport_factors)
+np.save(os.path.join(files,"testset_factors{}.npy".format(seed)),xport_factors_np)
 
-
-
+#exporting testidx track identifiers we wish to replace their track factors for
+with open (os.path.join(files,"testidx{}.pickle".format(seed)),'wb') as handle:
+    pickle.dump(testidx,handle)
 
 
 #x = eval(train_sparse,test_sparse,logmodel.user_factors,logmodel.item_factors,sparsify_out["userdropped"])
